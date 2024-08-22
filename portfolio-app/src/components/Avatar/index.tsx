bf5fff933326f8ebf0955ad96959eeb3a7113f44
saulopm <saulopmarinho@terra.com.br>
2024-08-21 21:45:19 -0300

    estilizando a sidebar

---
+++ portfolio-app/src/components/Avatar/index.tsx
@@ -0,0 +1,5 @@
+import { FotoPerfil } from './styles'
+
+const Avatar = () => <FotoPerfil src="https://github.com/saulopm200.png" />
+
+export default Avatar
--- portfolio-app/src/containers/Sidebar/index.tsx
+++ portfolio-app/src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/saulopm200.png" />
-    <Titulo fontSize={20}>Saulo Pinheiro</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Saulo Pinheiro</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        saulopm200
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro front-end
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
